388773febc6f4e5cd96afa8e939eb83829acda37
John Brandon <jbrandon@gmail.com>
2015-06-19 21:45:51 -0700

    Added checks on proportions (areas) & ages at transition

--- PBR Netbeans/PBR_Errorcheck_module.f90
+++ PBR Netbeans/PBR_Errorcheck_module.f90
@@ -8,32 +8,54 @@
 ! Originally compiled using: GCC GNU gfortran v 4.2 (free open source Fortran 90/95 compiler) -- https://gcc.gnu.org/
 ! IDE: Netbeans 8.0.2
 !====== +++ === === +++ === === +++ === ===
-
 MODULE PBR_Errorcheck_module
-    use Declare_variables_module ! Global variable declarations
+    use Declare_variables_module ! Global variable declarations -- mostly contains the variables read from the input file. 
     implicit none
     contains
-    
+!====== +++ === === +++ === === +++ === ===    
     integer(kind = 4) function error_check_input()
+!====== +++ === === +++ === === +++ === ===    
 ! Do some preliminary error checking on the input file 
 ! For example, the number of stocks entered should be either 1 or 2 (no less and no more)    
+!  This error checking code is a work in progress. It is certainly not yet as comprehensive as it could / should be.     
+!====== +++ === === +++ === === +++ === ===    
         error_check_input = 0
 
         if (n_stocks > 2 .or. n_stocks < 1) then      ! Check that number of stocks is supported
-            print *, "ERROR : Code only currently supports either one or two stocks. Check input file, input.par"
+            print *, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+            print *, "ERROR : Code only currently supports either one or two stocks. Check input file."
             error_check_input = -9
+            print *, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
         end if
 
         if (p_a1_s1 + p_a2_s1 .ne. 1.0) then
-            print *, "ERROR : Percentages for stock 1 in each area do not add to 1.0. Check input file, input.par"
+            print *, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+            print *, "ERROR : Percentages for stock 1 in each area do not add to 1.0. Check input file."
             error_check_input = -8            
+            print *, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
         end if
 
         if (p_a2_s2 + p_a3_s2 + p_a4_s2 .ne. 1.0) then
-            print *, "ERROR : Percentages for stock 2 in each area do not add to 1.0. Check input file, input.par"
+            print *, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+            print *, "ERROR : Percentages for stock 2 in each area do not add to 1.0. Check input file."
             error_check_input = -8            
+            print *, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
         end if
-
+        
+        if (a_m > age_x .or. a_t > age_x) then
+            print *, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+            print *, "ERROR : age_x must be greater than or equal to a_m and a_t. Check input file."
+            error_check_input = -7            
+            print *, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        end if
+        
+        if (a_t < 1 .or. a_t > age_x) then
+            print *, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+            print *, "ERROR : Condition (1 < a_t <= age_x) not met. Check input file."
+            error_check_input = -7            
+            print *, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        end if
+        
         return
     end function error_check_input
 
