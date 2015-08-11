8a3dd1d1e58a91a11e5af6366cb81685998da08e
John Brandon <jbrandon@gmail.com>
2015-08-11 13:23:44 -0700

    Cleaned up Declare_variables_module

--- PBR Netbeans/Declare_variables_module.f90
+++ PBR Netbeans/Declare_variables_module.f90
@@ -10,13 +10,9 @@
 !====== +++ === === +++ === === +++ === ===
 ! Purpose : Declare variables for PBR Tier System code
 !  These variables will be available to any routine that "uses" this module
-!  This approach is analogous with an "import" statement in Python (except variables, rather than functions), not global per se.
-!  It is not necessarily great programming practice. Could be improved, I'm sure. #OpenSource #SorryNotSorry 
 !====== +++ === === +++ === === +++ === ===
 MODULE Declare_variables_module
     implicit none
-!    INTEGER IOUT, IN1       ! File IO 
-!    DATA IOUT/8/,IN1/10/    ! File unit numbers for file IO
 !====== +++ === === +++ === === +++ === ! Variables read from input.par file
     CHARACTER REF*10,DESC*50            ! Reference tag for scenario and brief description: Naming adopted from AEP's code for File IO
     character cseed*1                   ! Should the seed for the RNG be user specified (Y) or generated from CPU clock (N)
@@ -24,19 +20,19 @@ MODULE Declare_variables_module
     integer(kind = 4) :: n_sims         ! Number of simulated population projections
     integer(kind = 4) :: n_stocks       ! Number of stocks to model (at present, either 1 or 2 stocks) 
     integer(kind = 4) :: yr_max         ! Number of years to project over   
-    integer(kind = 4) :: surv_freq   ! Interval (yrs) between abundance surveys (first abundance survey in year 1)    
+    integer(kind = 4) :: surv_freq      ! Interval (yrs) between abundance surveys (first abundance survey in year 1)    
     real(kind = 8) :: k_1plus(2)        ! Carrying capacity (in terms of the age 1+ component of the population)    
-    real(kind = 8) :: cv_n                ! The coefficient of variation (CV) associated with sampling error in abundance estimates
-    real(kind = 8) :: cv_mortality(2)     ! The CV associated with sampling error for human caused mortality of marine mammals
-    real(kind = 8) :: theta               ! The shape parameter for density dependence (assumed to act through birth rates)
-    real(kind = 8) :: r_max               ! The maximum annual growth rate of the population
-    real(kind = 8) :: f_r(2)              ! Recovery factor parameter for PBR    
-    real(kind = 8) :: init_depl(2)        ! Initial depletion (fraction of carrying capacity) in first year of projections
-    real(kind = 8) :: lower_tail          ! The percentile of the (log-normal) abundance estimates = N_min for PBR
-    real(kind = 8) :: b_max               ! Maximum birth rate (in the absence of carrying capacity, i.e. in the limit of zero abundance)
-    real(kind = 8) :: b_sex_ratio         ! Sex ratio at birth, in terms of female calves per female (i.e. 0.75 -> 3/4 calves are female)
-    real(kind = 8) :: s_adult             ! Adult survival rate
-    real(kind = 8) :: s_juv               ! Juvenile survival rate
+    real(kind = 8) :: cv_n              ! The coefficient of variation (CV) associated with sampling error in abundance estimates
+    real(kind = 8) :: cv_mortality(2)   ! The CV associated with sampling error for human caused mortality of marine mammals
+    real(kind = 8) :: theta             ! The shape parameter for density dependence (assumed to act through birth rates)
+    real(kind = 8) :: r_max             ! The maximum annual growth rate of the population
+    real(kind = 8) :: f_r(2)            ! Recovery factor parameter for PBR    
+    real(kind = 8) :: init_depl(2)      ! Initial depletion (fraction of carrying capacity) in first year of projections
+    real(kind = 8) :: lower_tail        ! The percentile of the (log-normal) abundance estimates = N_min for PBR
+    real(kind = 8) :: b_max             ! Maximum birth rate (in the absence of carrying capacity, i.e. in the limit of zero abundance)
+    real(kind = 8) :: b_sex_ratio       ! Sex ratio at birth, in terms of female calves per female (i.e. 0.75 -> 3/4 calves are female)
+    real(kind = 8) :: s_adult           ! Adult survival rate
+    real(kind = 8) :: s_juv             ! Juvenile survival rate
     integer(kind = 4) :: a_t            ! Age at transition to adult survival 
     integer(kind = 4) :: a_m            ! Age at sexual maturity  
     integer(kind = 4) :: age_x          ! Age at which individuals enter the 'plus-group' (identical for each stock at present)    
@@ -58,49 +54,29 @@ MODULE Declare_variables_module
     real(kind = 8), allocatable :: NPR_age(:)       ! Numbers-at-age-per-recruit
     real(kind = 8), allocatable :: NPR_age_tmp(:)   ! Vector of numbers at age, used to solve for initial human caused mortality rate 
     real(kind = 8) :: NPR_oneplus                   ! Total numbers of ages 1+ per female recruit (zero human caused mortality)    
-!    real(kind=8) :: initial_oneplus                 ! Size of female 1+ component on per-recruit basis given F_init    
-!====== +++ === === +++ === === +++ === ! These maybe NOT so much     
+!    real(kind=8) :: initial_oneplus                ! Size of female 1+ component on per-recruit basis given F_init    
+!====== +++ === === +++ === === +++ === ! Variables below might not need to be in this module
     real(kind=8) :: b_eq                            ! Equilibrium birth rate (at carrying capacity)
     real(kind=8) :: b_init                          ! Birth rate in first year of projections (a function of initial depletion in abundance)
-    real(kind=8) :: fecundity_max        ! Female calves per female, for eigen analysis of projection matrix 
-!    integer(kind = 4) :: first_yr       ! First year of projection
+    real(kind=8) :: fecundity_max                   ! Female calves per female, for eigen analysis of projection matrix 
    
-    integer(kind = 4) :: stock_i            ! Stock ID number   
+    integer(kind = 4) :: stock_i                    ! Stock ID number   
 
-    real(kind = 8) :: N_best                ! Mean of the abundance estimate 
-    real(kind = 8), allocatable :: N_min_yr_sim(:, :)                 ! 
+    real(kind = 8) :: N_best                          ! Mean of the abundance estimate, E[N], given sampling error (CV_N)
+    real(kind = 8), allocatable :: N_min_yr_sim(:, :) ! N_min estimates by year and simulation number
     
-!    real(kind = 8) :: z_variate             ! Standard normal random variate, i.e. ~ N(mu = 0, sigma = 1)
-
-!    real(kind = 8) :: f_init ! Initial human caused mortality rate
-!    real(kind = 8) :: objf_f_init     ! Objective function value for solving for initial human caused mortality rate
-
-!    real(kind = 8) :: temp_1plus    !
-!    real(kind = 8) :: temp_mat      !
-!    real(kind = 8) :: temp_mature   ! 
- 
     real(kind = 8) :: sum_NPR       ! The sum across ages in the NPR vector (including age zero calves)
-    
-!    real(kind = 8) :: rec_init      ! Initial recruitment conditioned on Finit (in terms of females)
-!    real(kind = 8) :: pred_rec      ! Predicted recruitment given some value for F_init (used to solve for equilibrium F_init)
-        
+           
     real(kind = 8), allocatable :: Female_age(:)        ! Vector with numbers-at-age for females 
     real(kind = 8), allocatable :: Male_age(:)          ! Vector with numbers-at-age for males 
     real(kind = 8), allocatable :: Nage_imm_0(:)        ! numbers-at-age that are immature
     real(kind = 8), allocatable :: Nage_mat_0(:)        ! numbers-at-age that are mature
     real(kind = 8), allocatable :: prop_NPR(:)          ! Proportions in each age of the NPR vector
     
-    real(kind = 8), allocatable :: N_plus(:)    ! Vector of 1+ population size for stock 'i' across all years 
-    real(kind = 8), allocatable :: N_tot(:)        ! Total (0+) population size each year for stock 'i' 
-    real(kind = 8), allocatable :: N_calf(:)       ! Vector of calf production for stock 'i' across all years
-!    real(kind = 8), allocatable :: depl_i_t(:, :)         ! Depletion for each stock each year
-    real(kind = 8), allocatable :: N_age(:)       ! Numbers-at-age
-!    real(kind = 8), allocatable :: selectivity_i(:, :)    ! Selectivity at age for stock 'i'
-!    
-!    real(kind = 8), allocatable :: N_oneplus_i_t(:, :, :)    ! Vector of 1+ population size for stock 'i' across all years 
-!    real(kind = 8), allocatable :: N_tot_i_t(:, :, :)        ! Total (0+) population size each year for stock 'i' 
-!    real(kind = 8), allocatable :: N_calf_i_t(:, :, :)       ! Vector of calf production for stock 'i' across all years
-!    real(kind = 8), allocatable :: depl_i_t(:, :, :)         ! Depletion for each stock each year
+    real(kind = 8), allocatable :: N_plus(:)            ! Vector of 1+ population size for stock 'i' across all years 
+    real(kind = 8), allocatable :: N_tot(:)             ! Total (0+) population size each year for stock 'i' 
+    real(kind = 8), allocatable :: N_calf(:)            ! Vector of calf production for stock 'i' across all years
+    real(kind = 8), allocatable :: N_age(:)             ! Numbers-at-age
 
 !====== +++ === === +++ === === +++ === 
     contains
@@ -108,7 +84,7 @@ MODULE Declare_variables_module
     subroutine initialize_global_vars()
 !====== +++ === === +++ === === +++ ===   
 !###### +++ ### ### +++ ### ### +++ ###         
-! Initialize global variables above to zero. 
+! Initialize variables declared in this module. Set values zero. 
 ! Only do this for those variables with values not read by input file.            
 !====== +++ === === +++ === === +++ ===
         b_eq = 0.0d0                 ! Equilibrium birth rate (at carrying capacity)
@@ -129,14 +105,9 @@ MODULE Declare_variables_module
         N_plus = 0.0d0 
         N_tot = 0.0d0
         N_calf = 0.0d0
-        print *, "Hello from initialize_global_vars"
-!        depl_i_t = 0.0d0 ! This has not been allocated or used
-         
         N_age = 0.0d0
        
-        print *, "Goodbye from initialize_global_vars"
+        return
     end subroutine initialize_global_vars
-
-
-
+! *
 END MODULE Declare_variables_module
