cf986d0141bee9199bdb47e10b476940aa7fcd71
John Brandon <jbrandon@gmail.com>
2017-01-23 17:21:03 -0800

    Fig 2. Solutions for Nmin as function of CVn

--- PBR Netbeans/Nmin_vs_CVn_plot.R
+++ PBR Netbeans/Nmin_vs_CVn_plot.R
@@ -8,24 +8,14 @@ library(magrittr)
 library(dplyr)
 library(ggplot2)
 
-main_dir = getwd(); main_dir
-tmp_wdir = "~/Documents/2015 work/PBR Tier System/Code/pbrr/R"
-
-setwd(tmp_wdir)
-
 snmin = read.csv(file = "solve_Nmin.csv")
 snmin %<>% tbl_df() %>% mutate(Tier = as.factor(Tier))
-
-setwd(main_dir)
 
 # Plot N_min precentile solution as function of CV_N ---------------------------
 plt = ggplot(data = snmin, aes(x = CV, y = Nmin, group = Tier, fill = Tier, color = Tier)) # , color = Tier
 plt = plt + geom_line(lwd = 1.5) + mytheme_bw 
 plt = plt + geom_point(size = 5, aes(shape = Tier, color = Tier))
 # plt = plt + scale_color_manual(values = c("gray40", "gray60", "gray80"))  # For print version (B & W)
-# plt = plt + scale_color_manual(values = c("green", "blue", "orange"))
-# plt = plt + scale_color_manual(values = c("#1b9e77", "#d95f02", "#7570b3"))
-# plt = plt + scale_color_manual(values = c("#b2df8a", "#33a02c", "#1f78b4"))
 # plt = plt + scale_color_manual(values = c("#a6cee3", "#33a02c", "#1f78b4"))  # For web version (color)
 plt = plt + scale_color_manual(values = c("green", "blue", "black"))
 plt = plt + labs(y = expression(italic(N[MIN])*"  Percentile"), x = expression(CV[N]))
@@ -36,7 +26,7 @@ plt = plt + theme(legend.title=element_blank())
 plt
 
 # Save a high-res copy of the plot for publication
-fig_path = "~/Documents/2015 Work/PBR Tier System/Manuscript Text/Draft_for_journal/Revised Manuscript After Review/Revised-High-Res-Figs"
+fig_path = ~/YourDirectory/  # where you want ggsave to write output plot 
 fig_width = 9.58
 fig_height = 8.39
 ggsave(filename = "Figure_2_color.pdf", path = fig_path, width = fig_width, height = fig_height, dpi = 500)
