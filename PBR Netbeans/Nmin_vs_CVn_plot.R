2e0789c66473ba93ccac11833ebe9863b05ef405
John Brandon <jbrandon@gmail.com>
2017-01-23 20:24:04 -0800

    Tidied code. Added devtools::source_gist.

--- PBR Netbeans/Nmin_vs_CVn_plot.R
+++ PBR Netbeans/Nmin_vs_CVn_plot.R
@@ -1,29 +1,42 @@
 #
 # Plot N_min percentile that meets MMPA management objective as a function
 #  of the CV of the abundance estimates
+# John Brandon
+rm(list = ls())
+library(magrittr)  # For Pipes %>% in R
+library(dplyr)     # Data wrangling
+library(ggplot2)   # Plotting
+library(devtools)  # To source code snippet (aka 'gist') with custom plot theme
+#
+# Read in data.frame with solutions to Nmin percentile as function of CV_N -----
+#
+snmin = read.csv(file = "solve_Nmin.csv") %>% tbl_df() %>% 
+  mutate(Tier = as.factor(Tier))
+
+#
+# Plot N_min precentile solution as function of CV_N ---------------------------
 #
 
-# Read in data.frame with solutions to Nmin percentile as function of CV_N -----
-library(magrittr)
-library(dplyr)
-library(ggplot2)
+# Source R code from: https://gist.github.com/John-Brandon/484d152675507dd145fe
+# Use package devtools to load plot theme code with `source_gist`
+source_gist(id = "484d152675507dd145fe", filename = "mytheme_bw.R")
 
-snmin = read.csv(file = "solve_Nmin.csv")
-snmin %<>% tbl_df() %>% mutate(Tier = as.factor(Tier))
-
-# Plot N_min precentile solution as function of CV_N ---------------------------
-plt = ggplot(data = snmin, aes(x = CV, y = Nmin, group = Tier, fill = Tier, color = Tier)) # , color = Tier
-plt = plt + geom_line(lwd = 1.5) + mytheme_bw 
-plt = plt + geom_point(size = 5, aes(shape = Tier, color = Tier))
-# plt = plt + scale_color_manual(values = c("gray40", "gray60", "gray80"))  # For print version (B & W)
-# plt = plt + scale_color_manual(values = c("#a6cee3", "#33a02c", "#1f78b4"))  # For web version (color)
-plt = plt + scale_color_manual(values = c("green", "blue", "black"))
-plt = plt + labs(y = expression(italic(N[MIN])*"  Percentile"), x = expression(CV[N]))
-plt = plt + scale_x_continuous(breaks = seq(0.2, 1.0, by = 0.2))
-plt = plt + geom_vline(xintercept = 0.41, lty = 2)
-plt = plt + theme(legend.position = c(0.19, 0.85))
-plt = plt + theme(legend.title=element_blank())
-plt
+# Create plot 
+ggplot(data = snmin, 
+        aes(x = CV, 
+        y = Nmin, 
+        group = Tier, 
+        fill = Tier, 
+        color = Tier)) + 
+  geom_line(lwd = 1.5) + 
+  geom_point(size = 5, aes(shape = Tier, color = Tier)) +
+  mytheme_bw + 
+  scale_color_manual(values = c("green", "blue", "black")) +
+  labs(y = expression(italic(N[MIN])*"  Percentile"), x = expression(CV[N])) +
+  scale_x_continuous(breaks = seq(0.2, 1.0, by = 0.2)) +
+  geom_vline(xintercept = 0.41, lty = 2) + 
+  theme(legend.position = c(0.19, 0.85)) +
+  theme(legend.title=element_blank()) 
 
 # Save a high-res copy of the plot for publication
 fig_path = ~/YourDirectory/  # where you want ggsave to write output plot 
